54e88e9638b93164ff577ea98ebc1b8a26881f7a
tipb47 <79226893+tipb47@users.noreply.github.com>
2024-11-16 23:20:47 -0500

    idk

--- index.js
+++ index.js
@@ -223,8 +223,11 @@ function processTokenHolderUpdates(newHolders) {
   const updated = [];
   const removed = [];
 
+  print(len(newHolders.length) + " new holders")
+  print(len(tokenHolders.length) + " old holders")
+
   // newHolders and tokenHolders get compared. if newHolders is significantly lower than tokenHolders (meaning there was a pagination issue in the fetching of newHolders, lets say for example if we have 12000 in tokenHolders.length and newHolders.length is 11000, we have a problem. use ratios so this works at any holder count. since we fetch results in pages of 1000, assume that at any time during a fetch, it could fail.)
-  if (newHolders.length / tokenHolders.length < 0.95) {
+  if ((newHolders.length / tokenHolders.length) < 0.95) {
     console.error("Pagination issue detected.");
     return
   }
