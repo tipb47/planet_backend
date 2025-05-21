5cafd6aca6d09dd00a830c9560023f76d00dfcc4
tipb47 <79226893+tipb47@users.noreply.github.com>
2025-05-21 04:39:36 -0500

    delay

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ async function fetchTokenDecimals() {
 }
 
 // Set up the periodic fetch
-setInterval(() => updateTokenHolders(), 60000); // Every minute
+setInterval(() => updateTokenHolders(), 300000); // Every 5 minutes
 
 // Initial fetch to populate holders on startup and fetch decimals
 async function initialize() {
